22f3f6a83c0c488cf79f982299b791d28ce83b64
Annie Zheng <anniezheng.anniee@gmail.com>
2019-10-16 17:03:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout (string) {
+  return string
+}
+'Hello! ' .toUpperCase()
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string
+}
+'Hello! '.toLowerCase()
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  return string
+}
